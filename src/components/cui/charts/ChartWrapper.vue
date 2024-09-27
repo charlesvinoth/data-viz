d09501e809dafbe1df35f12d7e66426544ee9778
Charles Vinoth <charlezvinoth@gmail.com>
2024-09-28 00:56:42 +0530

    milestone 6

---
+++ src/components/cui/charts/ChartWrapper.vue
@@ -0,0 +1,99 @@
+<script setup lang="ts">
+import { useDark } from '@vueuse/core'
+import { BarChart, LineChart } from 'echarts/charts'
+import { GridComponent, LegendComponent, TooltipComponent } from 'echarts/components'
+import { use } from 'echarts/core'
+import { CanvasRenderer } from 'echarts/renderers'
+import colors from 'tailwindcss/colors'
+import { computed } from 'vue'
+import VChart from 'vue-echarts'
+
+const isDark = useDark()
+
+use([CanvasRenderer, BarChart, LineChart, TooltipComponent, LegendComponent, GridComponent])
+
+const option = computed(() => ({
+  tooltip: {
+    // trigger: 'axis',
+  },
+  legend: {
+    data: ['2023', '2024'],
+    show: true,
+    itemGap: 16,
+    padding: 16,
+    // itemWidth: 16,
+    // itemHeight: 16,
+    // borderRadius: 8,
+    textStyle: {
+      color: isDark.value ? colors.gray[400] : colors.gray[600],
+      fontWeight: 500,
+      fontFamily: 'Inter',
+      lineHeight: 16,
+    },
+  },
+  grid: {
+    show: false,
+  },
+  xAxis: {
+    axisLine: {
+      lineStyle: {
+        color: isDark.value ? colors.gray[800] : colors.gray[200],
+      },
+    },
+    axisTick: {
+      show: false,
+      alignWithLabel: true,
+    },
+    axisLabel: {
+      color: isDark.value ? colors.gray[400] : colors.gray[600],
+      fontFamily: 'Inter',
+      lineHeight: 16,
+    },
+
+    data: ['Shirts', 'Cardigans', 'Chiffons', 'Pants', 'Heels', 'Socks'],
+  },
+  yAxis: {
+    axisLine: {
+      show: true,
+      lineStyle: {
+        color: isDark.value ? colors.gray[800] : colors.gray[200],
+      },
+    },
+    axisTick: {
+      show: false,
+      alignWithLabel: true,
+    },
+    axisLabel: {
+      color: isDark.value ? colors.gray[400] : colors.gray[600],
+      fontFamily: 'Inter',
+      lineHeight: 16,
+    },
+    splitLine: {
+      show: true,
+      lineStyle: {
+        color: isDark.value ? colors.gray[800] : colors.gray[200],
+      },
+    },
+  },
+  series: [
+    {
+      name: '2023',
+      type: 'bar',
+      data: [5, 20, 36, 10, 10, 20],
+      color: [colors.blue[400]],
+    },
+    {
+      name: '2024',
+      type: 'bar',
+      data: [15, 25, 18, 5, 20, 30],
+      color: [colors.emerald[400]],
+    },
+  ],
+}))
+</script>
+
+<template>
+  <VChart :option="option" autoresize class="size-full" />
+</template>
+
+<style scoped></style>
--- src/pages/dashboard/DashboardPage.vue
+++ src/pages/dashboard/DashboardPage.vue
@@ -1,7 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ChartWrapper from '@/components/cui/charts/ChartWrapper.vue'
+</script>
 
 <template>
-  <div class="flex items-center gap-2 p-6"></div>
+  <div class="flex items-center justify-center p-6">
+    <div class="aspect-[16/9] h-96 rounded-md bg-white dark:bg-gray-900">
+      <ChartWrapper />
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
